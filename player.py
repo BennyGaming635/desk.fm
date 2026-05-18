9729121ebbe4912a24f6b5c4ddc687b1af96b401
Benjamin Graetz <benjamin.graetz@henleyhs.sa.edu.au>
2026-05-18 18:38:54 +0930

    add player.py audio engine

---
+++ player.py
@@ -0,0 +1,23 @@
+import vlc
+
+class MusicPlayer:
+    def __init__(self):
+        self.instance = vlc.Instance()
+        self.player = self.instance.media_player_new()
+        self.media = None
+
+    def load(self, path):
+        self.media = self.instance.media_new(path)
+        self.player.set_media(self.media)
+
+    def play(self):
+        self.player.play()
+
+    def pause(self):
+        self.player.pause()
+
+    def stop(self):
+        self.player.stop()
+    
+    def set_volume(self, volume):
+        self.player.audio_set_volume(volume)
