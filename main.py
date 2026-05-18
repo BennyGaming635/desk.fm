fb502b53a437feb1b9d2cd4bd3144dd05bea0361
Benjamin Graetz <benjamin.graetz@henleyhs.sa.edu.au>
2026-05-18 22:57:55 +0930

    add update to main.py

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ import sys
 from PySide6.QtWidgets import QApplication
 from player import MusicPlayer
 from ui import MusicUI
+from settings import check_github_update
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     player = MusicPlayer()
     window = MusicUI(player)
     window.show()
+
+    update = check_github_update()
+    if update:
+        print("Latest version:", update["version"])
 
     sys.exit(app.exec())
