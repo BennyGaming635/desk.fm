3b2b4379fd77215b646581cdd65eccdfccb01f84
Benjamin Graetz <benjamin.graetz@henleyhs.sa.edu.au>
2026-05-19 15:37:02 +0930

    make a handler to extrract the song metadata in utils

--- utils.py
+++ utils.py
@@ -22,3 +22,43 @@ def extract_cover_image(audio_path):
     except Exception as e:
         print(f"Error extracting cover from {audio_path}: {e}")
         return None
+    
+    def get_song_metadata(path):
+        audio = File(path)
+        title = None
+        artist = "Unknown Artist"
+        album = "Unknown Album"
+
+        try:
+            if audio.tags:
+                if "TIT2" in audio.tags:
+                    title = str(audio.tags["TIT2"])
+                
+                if "TPE1" in audio.tags:
+                    artist = str(audio.tags["TPE1"])
+
+                if "TALB" in audio.tags:
+                    album = str(audio.tags["TALB"])
+
+                if not title and "title" in audio:
+                    title = audio["title"][0]
+
+                if "artist" in audio:
+                    artist = audio["artist"][0]
+
+                if "album" in audio:
+                    album = audio["album"][0]
+
+        except Exception as e:
+            print(f"Error extracting metadata from {path}: {e}")
+
+        if not title:
+            title = os.path.splitext(
+                os.path.basename(path)
+            )[0]
+
+        return {
+            "title": title,
+            "artist": artist,
+            "album": album
+        }
