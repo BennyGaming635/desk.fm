7ab89d28775d69f451573a419f4aaddb65709983
Benjamin Graetz <benjamin.graetz@henleyhs.sa.edu.au>
2026-05-19 16:10:37 +0930

    use qicon in window icon

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import sys
 from PySide6.QtWidgets import QApplication
 from player import MusicPlayer
 from ui import MusicUI
+from PySide6.QtGui import QIcon
 from settings import check_github_update
 from library import init_db
 
 if __name__ == "__main__":
     init_db()
     app = QApplication(sys.argv)
-    app.setWindowIcon("assets/icon.png")
+    app.setWindowIcon(QIcon("assets/icon.png"))
     player = MusicPlayer()
     window = MusicUI(player)
     window.show()
