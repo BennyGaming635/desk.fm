7c29bdc865e629b43f6c8f328b5a1133b9f33df0
Benjamin Graetz <benjamin.graetz@henleyhs.sa.edu.au>
2026-05-20 20:49:52 +0930

    add playlist db, and methods to create playlists, add songs to playlists and get playlists

--- library.py
+++ library.py
@@ -19,6 +19,20 @@ def init_db():
         )
     """)
 
+    c.execute("""
+        CREATE TABLE IF NOT EXISTS playlists (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT UNIQUE
+        )
+        """)
+    
+    c.execute("""
+        CREATE TABLE IF NOT EXISTS playlist_songs (
+            playlist_id INTEGER,
+            song_path TEXT
+        )
+        """)
+
     conn.commit()
     conn.close()
 
@@ -53,6 +67,54 @@ def remove_song(path):
     c.execute(
         "DELETE FROM songs WHERE path = ?", (path,)
     )
+    conn.commit()
+    conn.close()
+
+def create_playlist(name):
+    conn = sqlite3.connect(DB_FILE)
+    c = conn.cursor()
+
+    c.execute(
+        "INSERT OR IGNORE INTO playlists(name) VALUES(?)",
+        (name,)
+    )
+    conn.commit()
+    conn.close()
+
+def get_playlists():
+    conn = sqlite3.connect(DB_FILE)
+    c = conn.cursor()
+    c.execute("SELECT id, name FROM playlists")
+    rows = c.fetchall()
+    conn.close()
+    return [r[0] for r in rows]
+
+def add_song_to_playlist(playlist_id, song_path):
+    conn = sqlite3.connect(DB_FILE)
+    c = conn.cursor()
+
+    c.execute(
+        "SELECT id FROM playlists WHERE name=?",
+        (playlist_id,)
+    )
+    result = c.fetchone()
+
+    if not result:
+        conn.close()
+        return
+    
+    playlist_id = result[0]
+
+    c.execute(
+        """
+        INSET INTO playlist_songs
+        (playlist_id, song_path)
+        VALUES (?, ?)
+        """,
+        (playlist_id, song_path)
+    )
 
     conn.commit()
     conn.close()
+    
+
