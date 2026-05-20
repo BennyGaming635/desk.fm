34d8b6f1f63ac00aa94142aedf8ff46e98339d2b
Benjamin Graetz <benjamin.graetz@henleyhs.sa.edu.au>
2026-05-20 20:35:46 +0930

    add a system tray icon with show and qui

--- main.py
+++ main.py
@@ -13,6 +13,21 @@ if __name__ == "__main__":
     player = MusicPlayer()
     window = MusicUI(player)
     window.show()
+    tray = QSystemTrayIcon()
+    tray.setIcon(QIcon("assets/icon.png"))
+    tray.setVisible(True)
+    tray_menu = QMenu()
+    show_action = QAction("Show")
+    quit_action = QAction("Quit")
+    
+    show_action.triggered.connect(window.show)
+    quit_action.triggered.connect(app.quit)
+
+    tray_menu.addAction(show_action)
+    tray_menu.addSeparator()
+    tray_menu.addAction(quit_action)
+
+    tray.setContextMenu(tray_menu)
 
     update = check_github_update()
     if update:
