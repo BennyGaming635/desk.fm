c65113c6ab54c5a14db80ba5d18683e59ba5c0be
Benjamin Graetz <benjamin.graetz@henleyhs.sa.edu.au>
2026-06-12 18:09:32 +0930

    update build script

--- build.py
+++ build.py
@@ -6,6 +6,8 @@ APP_NAME = "DeskFM"
 ENTRY_FILE = "main.py"
 ASSETS_DIR = "assets"
 
+VLC_PATH = r"C:\Program Files\VideoLAN\VLC"
+
 
 def clean():
     for folder in ["build", "dist", "__pycache__"]:
@@ -23,7 +25,25 @@ def build():
     ]
 
     if os.path.exists(ASSETS_DIR):
-        cmd += ["--add-data", f"{ASSETS_DIR}{os.pathsep}{ASSETS_DIR}"]
+        cmd += [
+            "--add-data",
+            f"{ASSETS_DIR}{os.pathsep}{ASSETS_DIR}"
+        ]
+
+    if os.path.exists(VLC_PATH):
+        cmd += [
+            "--add-binary",
+            f"{os.path.join(VLC_PATH, 'libvlc.dll')}{os.pathsep}.",
+            "--add-binary",
+            f"{os.path.join(VLC_PATH, 'libvlccore.dll')}{os.pathsep}.",
+        ]
+
+        plugins = os.path.join(VLC_PATH, "plugins")
+        if os.path.exists(plugins):
+            cmd += [
+                "--add-data",
+                f"{plugins}{os.pathsep}plugins"
+            ]
 
     subprocess.run(cmd)
 
